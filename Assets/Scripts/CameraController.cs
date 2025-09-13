dd6e37c72597eff0c0a07897bf24ce85a7d3b7ec
idontreallyexist <kevinalexyu09@gmail.com>
2025-09-13 13:42:39 -0700

    Fixed the Camera bug

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,7 +1,8 @@
-using UnityEditor.Timeline;
-using UnityEngine.InputSystem;
-using UnityEngine;
 using Unity.VisualScripting;
+using UnityEditor.Timeline;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class CameraController : MonoBehaviour
 {
@@ -9,7 +10,8 @@ public class CameraController : MonoBehaviour
     public Transform target;
     public Transform pivot;
     public Vector3 offset;
-    public float mouseSpeed = 5.0f;
+    public float mouseSpeed = 3.0f;
+    public float prevHorizontal = 0;
     void Start()
     {
         offset = target.transform.position-transform.position;
@@ -33,7 +35,8 @@ public class CameraController : MonoBehaviour
         if (pivot.eulerAngles.x > 60f && pivot.eulerAngles.x < 180f) {
             pivot.rotation = Quaternion.Euler(60f, 0, 0);
         }
-        target.Rotate(0, horizontal, 0);
+        target.Rotate(0, horizontal-prevHorizontal, 0);
+        prevHorizontal = horizontal;
         if (pivot.eulerAngles.x > 180f && pivot.eulerAngles.x < 330f)
         {
             pivot.rotation = Quaternion.Euler(330f, 0, 0);
